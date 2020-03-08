73def7cbaa2caf877162b8510f44ff18567ec7c1
mrbru3 <53625291+mrbru3@users.noreply.github.com>
2020-03-08 13:16:38 -0400

    Add files via upload

---
+++ StartReset/StartReset/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace StartReset
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(39, 84);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 70);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(165, 84);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 70);
+            this.buttonReset.TabIndex = 1;
+            this.buttonReset.Text = "RESET";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
+
---
+++ StartReset/StartReset/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace StartReset
+{
+    public partial class Form1 : Form
+    {
+        Thread thread;
+        ManualResetEvent mre;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //mre = new ManualResetEvent(false);
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            mre = new ManualResetEvent(false);
+            new Thread(() => 
+            {
+                bool state = mre.WaitOne(2000);
+                if (state == false) MessageBox.Show("Too Slow");//MessageBox.Show(state.ToString());
+
+            }).Start();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+
+            mre.Set();//Reset();//Set();
+        }
+    }
+}
